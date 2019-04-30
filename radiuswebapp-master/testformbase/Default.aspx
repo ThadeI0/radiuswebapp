f899ea1be6e10c1bddbc1d9244c837a0137f7dde
ThadeI0 <38001546+ThadeI0@users.noreply.github.com>
2019-04-30 14:25:04 +0300

    Add files via upload
    initial

---
+++ radiuswebapp-master/testformbase/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="testformbase.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    
+</head>
+<body>
+    <form id="form1" runat="server">
+        <br />
+        <asp:Label ID="lbIIp" runat="server" Text="ip:" AssociatedControlID="TextBox1"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        &nbsp;<asp:DropDownList ID="ddlVendor" runat="server">
+            <asp:ListItem>dlink</asp:ListItem>
+            <asp:ListItem>eltex</asp:ListItem>
+        </asp:DropDownList>
+        &nbsp;<asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Send" />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Server: "></asp:Label>
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ radiuswebapp-master/testformbase/Default.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Web;
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+
+
+namespace testformbase
+{
+        
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private dynamic jsonfile = JsonConvert.DeserializeObject(File.ReadAllText(HttpContext.Current.Server.MapPath(".") + @"\\" + "appconfig.json"));
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = "Server: ";
+            if (Request.QueryString["ip"] != null && Request.QueryString["ip"] != "") TextBox1.Text = Request.QueryString["ip"].Trim();
+            if (Request.QueryString["vendor"] == "eltex") ddlVendor.Items[1].Selected = true;
+            else ddlVendor.Items[0].Selected = true;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (TextBox1.Text == "") throw new Exception("ip cannot be blank or null");
+                MySqlCommand command = new MySqlCommand();
+                string connectionString, commandString;
+                connectionString = string.Format(@"Data source={0};UserId={1};Password={2};database={3};Connect Timeout={4}", jsonfile["host"].ToString(),
+                                                 jsonfile["user"].ToString(), jsonfile["password"].ToString(), jsonfile["database"].ToString(), jsonfile["timeout"].ToString()
+                                                );
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                if (ddlVendor.Text == "dlink")
+                {
+                    commandString = string.Format(jsonfile["dlink"].ToString(), TextBox1.Text);
+                }
+                else
+                {
+                    commandString = string.Format(jsonfile["eltex"], TextBox1.Text);
+                }
+                command.CommandText = commandString;
+                command.Connection = connection;
+                MySqlDataReader reader;
+                Stopwatch sw = new Stopwatch();
+                try
+                {
+                    
+                    sw.Start();
+                    command.Connection.Open();
+                    reader = command.ExecuteReader();
+                    var dataTable = new DataTable();
+                    dataTable.Load(reader);
+
+                    GridView1.DataSource = dataTable;
+                    GridView1.DataBind();
+                    reader.Close();
+                }
+                catch (MySqlException ex)
+                {
+                    
+                    Label1.Text += ex.Message.ToString();
+                }
+                finally
+                {
+                    sw.Stop();
+                    Label1.Text += sw.Elapsed.ToString();
+                    command.Connection.Close();
+                     
+                }
+            }
+            catch(Exception ex)
+            {
+                Label1.Text += ex.Message.ToString();
+            }
+            
+        }
+
+    }
+}
